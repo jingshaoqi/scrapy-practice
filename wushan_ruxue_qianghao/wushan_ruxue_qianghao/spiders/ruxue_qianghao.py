aa14beb11a63a8ce8899e005a92ff4e06187e55b
jingshaoqi <jingshaoqi@163.com>
2023-06-15 09:18:26 +0800

    修改学前抢号程序

--- wushan_ruxue_qianghao/wushan_ruxue_qianghao/spiders/ruxue_qianghao.py
+++ wushan_ruxue_qianghao/wushan_ruxue_qianghao/spiders/ruxue_qianghao.py
@@ -31,18 +31,52 @@ class RuxueQianghaoSpider(scrapy.Spider):
             school = itd[2].xpath('./text()')
             school_t = school.extract()[0]
             print(school_t)
-            if school_t.find('C校') < 0:
+            # 巫峡幼儿园 机关幼儿园 平湖幼儿园 西坪幼儿园 白杨幼儿园 圣泉幼儿园
+            if school_t.find('A校') < 0:
                 continue
             school_url = itd[6].xpath('./a/@href')
             school_url_t = school_url.extract()[0]
             print(school_url_t)
             school_url_full = urljoin(response.url, school_url_t)
+            # 输出请求的URL
+            print('school_url_full:{}'.format(school_url_full))
             # click to qianghao entrance
-            yield scrapy.Request(url=school_url_full, callback=self.school_parse)
+            #yield scrapy.Request(url=school_url_full, callback=self.school_parse_SFZH) #添加身份证号验证
+            yield scrapy.Request(url=school_url_full, callback=self.school_parse) #bu不添加身份证号验证
             break
 
+    def school_parse_SFZH(self, response):
+        with open('school_parse_SFZH.html', 'w') as f:
+            f.write(response.text)
+        # now should check identicard whether is normal
+        #print(response.text)
+        sfzh = response.xpath('//a[@id="LinkButtonSFZH"]')
+        sfzh_url = sfzh.xpath('./@href')
+        sfzh_url_t = sfzh_url.extract()[0]
+        print(sfzh_url_t)
+        # add data
+        fm_data = {'ScriptManager1': 'UpdatePanelAA|LinkButtonSFZH',
+                   '__EVENTTARGET':'TextBox_SFZH',
+                   'TextBox_SFZH': '500237202005319756',
+                   #'TextBox_XM': '',
+                   '__EVENTARGUMENT':'',
+                   '__LASTFOCUS':'',
+                   'TextBox_FJDZ': '重庆市巫山县高唐街道巫峡路110号4幢4单元1-2',
+                   'TextBox_JZDZ': '重庆市巫山县高唐街道巫峡路110号4幢4单元1-2',
+                   'TextBox_JFRXM': '王大海',
+                   'TextBox_JFRSFZH': '500237198905319744',
+                  'TextBox_TelePhone': '13278904979',
+                   '__ASYNCPOST':'true'}
+        yield scrapy.Request(url=response.url, method="POST", body=json.dumps(fm_data),callback=self.SFZH_check)
+
+    def SFZH_check(self, response):
+        with open('sfzh_check_sfzh.html', 'w') as f:
+            f.write(response.text)
+        #print(response.text)
+        self.school_parse(response)
+
     def school_parse(self, response):
-        with open('school.html', 'w') as f:
+        with open('school_parse.html', 'w') as f:
             f.write(response.text)
         # now should check identicard whether is normal
         print(response.text)
@@ -51,13 +85,22 @@ class RuxueQianghaoSpider(scrapy.Spider):
         sfzh_url_t = sfzh_url.extract()[0]
         print(sfzh_url_t)
         # add data
-        fm_data = {'ScriptManager1': 'UpdatePanelAA|LinkButtonSFZH', 'TextBox_SFZH': '500237202005319756',
-                  'TextBox_XM': '', 'TextBox_FJDZ': '', 'TextBox_JZDZ': '', 'TextBox_JFRXM': '', 'TextBox_JFRSFZH': '',
-                  'TextBox_TelePhone': ''}
-        yield scrapy.Request(url=response.url, method="POST", body=json.dumps(fm_data),callback=self.SFZH_check)
+        fm_data = {#'ScriptManager1': 'UpdatePanelAA|LinkButtonSFZH',
+                   '__EVENTTARGET':'TextBox_SFZH',
+                   'TextBox_SFZH': '500237202005319756',
+                   #'TextBox_XM': '',
+                   '__EVENTARGUMENT':'',
+                   '__LASTFOCUS':'',
+                   'TextBox_FJDZ': '重庆市巫山县高唐街道巫峡路110号4幢4单元1-2',
+                   'TextBox_JZDZ': '重庆市巫山县高唐街道巫峡路110号4幢4单元1-2',
+                   'TextBox_JFRXM': '王大海',
+                   'TextBox_JFRSFZH': '500237198905319744',
+                  'TextBox_TelePhone': '13278904979',
+                   '__ASYNCPOST':'true'}
+        yield scrapy.Request(url=response.url, method="POST", body=json.dumps(fm_data),callback=self.submit_info)
 
-    def SFZH_check(self, response):
-        with open('sfzh_check.html', 'w') as f:
+    def submit_info(self, response):
+        with open('submit_info.html', 'w') as f:
             f.write(response.text)
         print(response.text)
 
