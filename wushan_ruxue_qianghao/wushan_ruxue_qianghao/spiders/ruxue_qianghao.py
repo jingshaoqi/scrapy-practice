688045860ce0ea1950cfcb170ec3114d5d3c0da2
jingshaoqi <jingshaoqi@163.com>
2023-06-17 21:04:35 +0800

    完善学前入学抢号代码

--- wushan_ruxue_qianghao/wushan_ruxue_qianghao/spiders/ruxue_qianghao.py
+++ wushan_ruxue_qianghao/wushan_ruxue_qianghao/spiders/ruxue_qianghao.py
@@ -1,15 +1,28 @@
 import scrapy
 import  json
 from urllib.parse import urljoin
+import time
+from urllib.parse import urlencode
 
 class RuxueQianghaoSpider(scrapy.Spider):
     name = 'ruxue_qianghao'
     allowed_domains = ['wsemal.com']
     start_urls = ['https://wsemal.com/XQZS/JW/JW_iframe.aspx']
-    headers = {'host': 'wsemal.com',
-               'cookie': '',
-               'Connection':'keep-alive',
-               'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 Edg/114.0.1823.43'}
+    headers = {'Referer': 'https://wsemal.com/CZBM/',
+               'Host': 'wsemal.com',
+               'Cookie': '',
+               "User-Agent": "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/114.0",
+               "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+               "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
+               "Accept-Encoding": "gzip, deflate, br",
+               "Connection": "keep-alive",
+               "Upgrade-Insecure-Requests": "1",
+               "Sec-Fetch-Dest": "document",
+               "Sec-Fetch-Mode": "navigate",
+               "Sec-Fetch-Site": "none",
+               "Sec-Fetch-User": "?1",
+               "TE": "trailers",
+               }
     form_data = {'__EVENTTARGET': '',
                  '__EVENTARGUMENT': '',
                  '__VIEWSTATE': '',
@@ -17,22 +30,49 @@ class RuxueQianghaoSpider(scrapy.Spider):
                  '__EVENTVALIDATION': ''
                  }
 
+    def start_requests(self):
+        # wait
+        desttime = '2023-6-17 8:00:00'
+        a2time = time.strptime(desttime, '%Y-%m-%d %H:%M:%S')
+        while 1:
+            localtm = time.localtime()
+            if localtm >= a2time:
+                break
+            else:
+                time.sleep(0.01)
+        if self.headers.get('Cookie') is not None:
+            self.headers.pop('Cookie')
+        if self.headers.get('Referer') is not None:
+            self.headers.pop('Referer')
+        if self.headers.get('TE') is not None:
+            self.headers.pop('TE')
+
+        for url in self.start_urls:
+            yield scrapy.Request(url=url, headers=self.headers, callback=self.parse, dont_filter=True)
+
     def parse(self, response):
-        with open('ruxue.html', 'w') as f:
+        with open('JW_iframe.html', 'w') as f:
             f.write(response.text)
-        items = response.xpath('//iframe[contains(@src, "main")]/@src')
-        ckie = response.headers['Set-Cookie']
-        fdf = str(ckie).split(';')
-        # 只需要第一个
-        str_cookie = fdf[0]
-        self.headers['cookie'] = str_cookie
+        #items = response.xpath('//iframe[contains(@src, "main")]/@src')
+        items = response.xpath('//iframe[@id="mainFrame"]/@src')
+        if items is None:
+            return
+        if response.headers.get('Set-Cookie'):
+            ckie = response.headers['Set-Cookie']
+            fdf = str(ckie).split(';')
+            # 只需要第一个
+            for i in fdf:
+                if i.find('SessionId') >= 0:
+                    self.headers['Cookie'] = i  # ASP.NET_SessionId=3rg5mw45ldudcbmsvfnyayj0
+                    break
+
         self.headers['referer'] = response.url  # https://wsemal.com/XQZS/JW/JW_iframe.aspx
-        for i in items:
-            end_pr = i.extract()
-            main_url = urljoin(response.url, end_pr) #https://wsemal.com/XQZS/JW/JWmain.aspx
-            print(main_url)
-            # go to jw main.aspx web
-            yield scrapy.Request(url=main_url, headers=self.headers, callback=self.JWmain)
+        end_pr = items.get()
+        main_url = urljoin(response.url, end_pr)  # https://wsemal.com/XQZS/JW/JWmain.aspx
+        print(main_url)
+        # go to jw main.aspx web
+        yield scrapy.Request(url=main_url, headers=self.headers, callback=self.JWmain)
+
 
     def JWmain(self, response):
         with open('JWmain.html', 'w') as f:
@@ -45,22 +85,22 @@ class RuxueQianghaoSpider(scrapy.Spider):
                 continue
             print('has seven td')
             school = itd[2].xpath('./text()')
-            school_t = school.extract()[0]
+            school_t = school.get()
             print(school_t)
             # 巫峡幼儿园 机关幼儿园 平湖幼儿园 西坪幼儿园 白杨幼儿园 圣泉幼儿园
             if school_t.find('巫峡幼儿园') < 0:
                 continue
             school_url = itd[6].xpath('./a/@href')
-            school_url_t = school_url.extract()[0]
+            school_url_t = school_url.get()
             print(school_url_t)
             # https://wsemal.com/XQZS/JW/JW_ZSBM1.aspx?XX=XQ001&ID=13&TT=2023/6/18^%^209:00:00
-            school_url_full = urljoin(response.url, school_url_t)
+            qh_rukou_url_full = urljoin(response.url, school_url_t)
             # 输出请求的URL
-            print('school_url_full:{}'.format(school_url_full))
+            print('school_url_full:{}'.format(qh_rukou_url_full))
             # click to qianghao entrance
             self.headers['referer'] = response.url
             #yield scrapy.Request(url=school_url_full, callback=self.school_parse_SFZH) #添加身份证号验证
-            yield scrapy.Request(url=school_url_full, headers=self.headers, callback=self.school_parse) #bu不添加身份证号验证
+            yield scrapy.Request(url=qh_rukou_url_full, headers=self.headers, callback=self.qh_rukou_parse) #bu不添加身份证号验证
             break
 
     def school_parse_SFZH(self, response):
@@ -91,10 +131,10 @@ class RuxueQianghaoSpider(scrapy.Spider):
         with open('sfzh_check_sfzh.html', 'w') as f:
             f.write(response.text)
         #print(response.text)
-        self.school_parse(response)
+        self.qh_rukou_parse(response)
 
-    def school_parse(self, response):
-        with open('school_parse.html', 'w') as f:
+    def qh_rukou_parse(self, response):
+        with open('qh_rukou_parse.html', 'w') as f:
             f.write(response.text)
         # now should check identicard whether is normal
         # print(response.text)
@@ -114,6 +154,7 @@ class RuxueQianghaoSpider(scrapy.Spider):
         event_validation_str = event_validation.extract()[0] if len(event_validation) > 0 else ''
         view_state_generator = response.xpath('//input[@id="__VIEWSTATEGENERATOR"]/@value')
         view_state_generator_str = view_state_generator.extract()[0] if len(view_state_generator) > 0 else ''
+        # self.form_data['__EVENTTARGET'] = event_target_str 可能还得添加一行
         self.form_data['__EVENTTARGET'] = event_target_str
         self.form_data['__EVENTARGUMENT'] = event_argument_str
         self.form_data['__VIEWSTATE'] = view_state_str
@@ -128,7 +169,12 @@ class RuxueQianghaoSpider(scrapy.Spider):
         self.form_data['TextBox_JFRSFZH'] = '500237198905319744'
         self.form_data['TextBox_TelePhone'] = '13278904979'
         self.form_data['__ASYNCPOST'] = 'true'
-        yield scrapy.Request(url=response.url, method="POST", body=json.dumps(self.form_data), headers=self.headers, callback=self.submit_info)
+        self.form_data['submit'] = '提交' #这行的代码得改
+        bodystr = urlencode(self.form_data)
+        self.headers['Content-Type'] = 'application/x-www-form-urlencoded'
+        self.headers['Content-Length'] = '{}'.format(len(bodystr))
+
+        yield scrapy.Request(url=response.url, method="POST", body=bodystr, headers=self.headers, callback=self.submit_info)
 
     def submit_info(self, response):
         with open('submit_info.html', 'w') as f:
