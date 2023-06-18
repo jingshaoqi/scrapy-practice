c40c0f6a4341a43f88f4d23753eab32f0c19197c
jingshaoqi <jingshaoqi@163.com>
2023-06-18 15:58:48 +0800

    去掉不需要的代码

--- wushan_ruxue_qianghao/wushan_ruxue_qianghao/spiders/ruxue_qianghao.py
+++ wushan_ruxue_qianghao/wushan_ruxue_qianghao/spiders/ruxue_qianghao.py
@@ -4,6 +4,7 @@ import json
 from urllib.parse import urljoin
 import time
 from urllib.parse import urlencode
+import datetime
 
 class RuxueQianghaoSpider(scrapy.Spider):
     name = 'ruxue_qianghao'
@@ -33,8 +34,11 @@ class RuxueQianghaoSpider(scrapy.Spider):
     jw_main_url = ''
     jw_main_headers = {}
 
+    s_time = datetime.datetime.now()
+
     def start_requests(self):
         # wait
+        self.s_time = datetime.datetime.now()
         desttime = '2023-6-17 8:00:00'
         a2time = time.strptime(desttime, '%Y-%m-%d %H:%M:%S')
         while 1:
@@ -108,71 +112,7 @@ class RuxueQianghaoSpider(scrapy.Spider):
             # click to qianghao entrance
             self.headers['Referer'] = response.url
             yield scrapy.Request(url=qh_rukou_url_full, headers=self.headers, callback=self.qh_ru_kou_check, dont_filter=True) #添加身份证号验证
-            #yield scrapy.Request(url=qh_rukou_url_full, headers=self.headers, callback=self.qh_ru_kou_no_check,dont_filter=True) #不添加身份证号校验
             break
-
-    def qh_ru_kou_no_check(self, response):
-        with open('qh_ru_kou_no_check.html', 'w') as f:
-            f.write(response.text)
-            # 提取formdata信息
-        # 查看状态
-        zt = response.xpath('//tr/td/span[@id="Label_ZT"]/text()')
-        if zt is not None:
-            zt_s = zt.get()
-            if zt_s.find('进行中') < 0:
-                #点击返回
-                print('状态是:{}'.format(zt_s))
-                time.sleep(0.2)
-                yield scrapy.Request(url=self.jw_main_url, headers=self.jw_main_headers, callback=self.JWmain, dont_filter=True)
-                return
-
-        event_target = response.xpath('//input[@id="__EVENTTARGET"]/@value')
-        event_target_str = event_target.extract()[0] if len(event_target) > 0 else ''
-        event_argument = response.xpath('//input[@id="__EVENTARGUMENT"]/@value')
-        event_argument_str = event_argument.extract()[0] if len(event_argument) > 0 else ''
-        view_state = response.xpath('//input[@id="__VIEWSTATE"]/@value')
-        view_state_str = view_state.extract()[0] if len(view_state) > 0 else ''
-        event_validation = response.xpath('//input[@id="__EVENTVALIDATION"]/@value')
-        event_validation_str = event_validation.extract()[0] if len(event_validation) > 0 else ''
-        view_state_generator = response.xpath('//input[@id="__VIEWSTATEGENERATOR"]/@value')
-        view_state_generator_str = view_state_generator.extract()[0] if len(view_state_generator) > 0 else ''
-
-        self.form_data['ScriptManager1'] = 'UpdatePanelAA|Button1'
-        self.form_data['__EVENTTARGET'] = ''
-        self.form_data['__EVENTARGUMENT'] = event_argument_str
-        self.form_data['__VIEWSTATE'] = view_state_str
-        self.form_data['__EVENTVALIDATION'] = event_validation_str
-        self.form_data['__VIEWSTATEGENERATOR'] = view_state_generator_str
-        self.form_data['__LASTFOCUS'] = ''
-        self.form_data['TextBox_XM'] = '王秀丽'
-        self.form_data['TextBox_SFZH'] = '500237202005010142'
-        self.form_data['TextBox_FJDZ'] = '重庆市巫山县高唐街道巫峡路110号4幢4单元1-2'
-        self.form_data['TextBox_JZDZ'] = '重庆市巫山县高唐街道巫峡路110号4幢4单元1-2'
-        self.form_data['TextBox_JFRXM'] = '王大海'
-        self.form_data['TextBox_JFRSFZH'] = '500237198905319744'
-        self.form_data['TextBox_TelePhone'] = '13278904979'
-        self.form_data['__ASYNCPOST'] = 'true'
-        self.form_data['Button1'] = '提交申请'
-
-        self.headers['Referer'] = response.url
-        self.headers['X-Requested-With'] = 'XMLHttpRequest'
-        self.headers['X-MicrosoftAjax'] = 'Delta=true'
-        self.headers['Cache-Control'] = 'no-cache'
-        self.headers['Sec-Fetch-Dest'] = 'empty'
-        self.headers['Sec-Fetch-Mode'] = 'cors'
-        self.headers['Sec-Fetch-Site'] = 'same-origin'
-        self.headers['TE'] = 'trailers'
-        self.headers['Accept'] = '*/*'
-        self.headers['Origin'] = 'https://wsemal.com'
-        if self.headers.get('Upgrade-Insecure-Requests') is not None:
-            self.headers.pop('Upgrade-Insecure-Requests')
-
-        bodystr = urlencode(self.form_data, encoding='utf-8')
-        self.headers['Content-Type'] = 'application/x-www-form-urlencoded; charset=utf-8'
-        self.headers['Content-Length'] = '{}'.format(len(bodystr))
-
-        yield scrapy.Request(url=response.url, method="POST", body=bodystr, headers=self.headers,
-                             callback=self.submit_info, dont_filter=True)
 
     def qh_ru_kou_check(self, response):
         with open('qh_ru_kou_check.html', 'w') as f:
@@ -208,7 +148,7 @@ class RuxueQianghaoSpider(scrapy.Spider):
         self.form_data['__VIEWSTATEGENERATOR'] = view_state_generator_str
         self.form_data['__LASTFOCUS'] = ''
         self.form_data['TextBox_XM'] = ''
-        self.form_data['TextBox_SFZH'] = '500237202005010142'
+        self.form_data['TextBox_SFZH'] = '500237202005319879'
         self.form_data['TextBox_FJDZ'] = ''
         self.form_data['TextBox_JZDZ'] = ''
         self.form_data['TextBox_JFRXM'] = ''
@@ -251,7 +191,7 @@ class RuxueQianghaoSpider(scrapy.Spider):
         self.form_data['__EVENTARGUMENT'] = ''
         self.form_data['__LASTFOCUS'] = ''
         self.form_data['TextBox_XM'] = '王秀丽'
-        self.form_data['TextBox_SFZH'] = '500237202005010142'
+        self.form_data['TextBox_SFZH'] = '500237202005319879'
         self.form_data['TextBox_FJDZ'] = '重庆市巫山县高唐街道巫峡路110号4幢4单元1-2'
         self.form_data['TextBox_JZDZ'] = '重庆市巫山县高唐街道巫峡路110号4幢4单元1-2'
         self.form_data['TextBox_JFRXM'] = '王大海'
@@ -283,6 +223,9 @@ class RuxueQianghaoSpider(scrapy.Spider):
     def submit_info(self, response):
         with open('submit_info.html', 'w') as f:
             f.write(response.text)
+        e_time = datetime.datetime.now()
+        c=e_time-self.s_time
+        print('cost:{}'.format(c))
         print(response.text)
 
 
