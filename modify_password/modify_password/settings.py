488ec2be2215f5049d28bd810a12e4eaa639c29f
jingshaoqi <jingshaoqi@163.com>
2023-06-19 22:17:30 +0800

    简化代码，去掉多余的自定义head

--- modify_password/modify_password/settings.py
+++ modify_password/modify_password/settings.py
@@ -15,6 +15,7 @@ NEWSPIDER_MODULE = "modify_password.spiders"
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = "modify_password (+http://www.yourdomain.com)"
+USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/114.0"
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
--- modify_password/modify_password/spiders/modifypassworld.py
+++ modify_password/modify_password/spiders/modifypassworld.py
@@ -11,29 +11,7 @@ class ModifypassworldSpider(scrapy.Spider):
     name = "modifypassworld"
     allowed_domains = ["www.baidu.com"]
     start_urls = ["https://wsemal.com/CZBM/JW/JW_iframe.aspx?FS=CC"]
-
-    headers = {'Referer': 'https://wsemal.com/CZBM/',
-               'Host': 'wsemal.com',
-               'Cookie': '',
-               "User-Agent": "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/114.0",
-               "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-               "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
-               "Accept-Encoding": "gzip, deflate, br",
-               "Connection": "keep-alive",
-               "Upgrade-Insecure-Requests": "1",
-               "Sec-Fetch-Dest": "document",
-               "Sec-Fetch-Mode": "navigate",
-               "Sec-Fetch-Site": "none",
-               "Sec-Fetch-User": "?1",
-               "TE": "trailers",
-               }
-    form_data = {'__EVENTTARGET': '',
-                 '__EVENTARGUMENT': '',
-                 '__VIEWSTATE': '',
-                 '__VIEWSTATEGENERATOR': '',
-                 '__EVENTVALIDATION': '',
-                 '__LASTFOCUS': '',
-                 }
+    form_data = {}
     cur_user_pwd = ''  # 当前用户的密码
     user_dll_url = ''
 
@@ -47,15 +25,9 @@ class ModifypassworldSpider(scrapy.Spider):
                 break
             else:
                 time.sleep(0.01)
-        if self.headers.get('Cookie') is not None:
-            self.headers.pop('Cookie')
-        if self.headers.get('Referer') is not None:
-            self.headers.pop('Referer')
-        if self.headers.get('TE') is not None:
-            self.headers.pop('TE')
 
         for url in self.start_urls:
-            yield scrapy.Request(url=url, headers=self.headers, callback=self.parse, dont_filter=True)
+            yield scrapy.Request(url=url,  callback=self.parse, dont_filter=True)
 
     def parse(self, response):
         # find mainframe
@@ -66,33 +38,10 @@ class ModifypassworldSpider(scrapy.Spider):
         # https://wsemal.com/CZBM/JW/JW_UserDL.aspx
         main_frm_url_full = urljoin(response.url, main_frm_url)
         print(main_frm_url_full)
-        # 获取cookie
-        if response.headers.get('Set-Cookie'):
-            ckie = response.headers['Set-Cookie']
-            fdf = str(ckie).split(';')
-            # 只需要第一个
-            for i in fdf:
-                if i.find('SessionId') >= 0:
-                    self.headers['Cookie'] = i  # ASP.NET_SessionId=3rg5mw45ldudcbmsvfnyayj0
-                    break
-        self.headers['Referer'] = response.url  # https://wsemal.com/CZBM/JW/JW_iframe.aspx?FS=CC
-        self.headers['Sec-Fetch-Dest'] = 'iframe'
-        self.headers['Sec-Fetch-Mode'] = 'navigate'
-        self.headers['Sec-Fetch-Site'] = 'same-origin'
-        self.headers['TE'] = 'trailers'
-        # self.headers['Sec-Fetch-User'] = '?1'
+        headers = {}
+        headers['Referer'] = response.url  # https://wsemal.com/CZBM/JW/JW_iframe.aspx?FS=CC
         self.user_dll_url = main_frm_url_full  # https://wsemal.com/CZBM/JW/JW_UserDL.aspx
-        if self.headers.get('Content-Type') is not None:
-            self.headers.pop('Content-Type')
-        if self.headers.get('Content-Length') is not None:
-            self.headers.pop('Content-Length')
-        if self.headers.get('X-MicrosoftAjax') is not None:
-            self.headers.pop('X-MicrosoftAjax')
-        if self.headers.get('Origin') is not None:
-            self.headers.pop('Origin')
-        if self.headers.get('Sec-Fetch-User') is not None:
-            self.headers.pop('Sec-Fetch-User')
-        yield scrapy.Request(url=main_frm_url_full, callback=self.main_parse, headers=self.headers, dont_filter=True)
+        yield scrapy.Request(url=main_frm_url_full, callback=self.main_parse,  dont_filter=True)
 
     def main_parse(self, response):
         with open('JW_UserDL.aspx.html', 'w') as f:
@@ -105,31 +54,19 @@ class ModifypassworldSpider(scrapy.Spider):
         yzm_url_t = yzm_url.extract()[0]
         self.yzm_url_full = urljoin(response.url, yzm_url_t)
         # https://wsemal.com/CZBM/JW/JW_UserDL.aspx
-        self.headers['Referer'] = self.user_dll_url
-        self.headers['Accept'] = 'image/avif,image/webp,*/*'
-        self.headers['Sec-Fetch-Dest'] = 'image'
-        self.headers['Sec-Fetch-Mode'] = 'no-cors'
-        self.headers['Sec-Fetch-Site'] = 'same-origin'
-        self.headers['TE'] = 'trailers'
-        if self.headers.get('Sec-Fetch-User') is not None:
-            self.headers.pop('Sec-Fetch-User')
+
         # 提取formdata信息
-        event_target = response.xpath('//div/input[@id="__EVENTTARGET"]/@value')
-        event_target_str = event_target.extract()[0] if len(event_target) > 0 else ''
-        event_argument = response.xpath('//div/input[@id="__EVENTARGUMENT"]/@value')
-        event_argument_str = event_argument.extract()[0] if len(event_argument) > 0 else ''
-        view_state = response.xpath('//div/input[@id="__VIEWSTATE"]/@value')
-        view_state_str = view_state.extract()[0] if len(view_state) > 0 else ''
-        event_validation = response.xpath('//div/input[@id="__EVENTVALIDATION"]/@value')
-        event_validation_str = event_validation.extract()[0] if len(event_validation) > 0 else ''
-        view_state_generator = response.xpath('//div/input[@id="__VIEWSTATEGENERATOR"]/@value')
-        view_state_generator_str = view_state_generator.extract()[0] if len(view_state_generator) > 0 else ''
-        self.form_data['__EVENTTARGET'] = event_target_str
-        self.form_data['__EVENTARGUMENT'] = event_argument_str
-        self.form_data['__VIEWSTATE'] = view_state_str
-        self.form_data['__EVENTVALIDATION'] = event_validation_str
-        self.form_data['__VIEWSTATEGENERATOR'] = view_state_generator_str
-        yield scrapy.Request(url=self.yzm_url_full, callback=self.yzm_parse, headers=self.headers, dont_filter=True)
+        event_target = response.xpath('//input[@id="__EVENTTARGET"]/@value')
+        self.form_data['__EVENTTARGET'] = event_target.extract()[0] if len(event_target) > 0 else ''
+        event_argument = response.xpath('//input[@id="__EVENTARGUMENT"]/@value')
+        self.form_data['__EVENTARGUMENT'] = event_argument.extract()[0] if len(event_argument) > 0 else ''
+        view_state = response.xpath('//input[@id="__VIEWSTATE"]/@value')
+        self.form_data['__VIEWSTATE'] = view_state.extract()[0] if len(view_state) > 0 else ''
+        event_validation = response.xpath('//input[@id="__EVENTVALIDATION"]/@value')
+        self.form_data['__EVENTVALIDATION'] = event_validation.extract()[0] if len(event_validation) > 0 else ''
+        view_state_generator = response.xpath('//input[@id="__VIEWSTATEGENERATOR"]/@value')
+        self.form_data['__VIEWSTATEGENERATOR'] = view_state_generator.extract()[0] if len(view_state_generator) > 0 else ''
+        yield scrapy.Request(url=self.yzm_url_full, callback=self.yzm_parse, dont_filter=True)
 
     # 解析验证码后按登录按钮
     def yzm_parse(self, response):
@@ -140,14 +77,6 @@ class ModifypassworldSpider(scrapy.Spider):
         ocr = ddddocr.DdddOcr()
         res = ocr.classification(response.body)
         print('验证码为:{}'.format(res))
-        self.headers['Accept'] = 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8'
-        self.headers['Origin'] = 'https://wsemal.com'
-        self.headers['Upgrade-Insecure-Requests'] = '1'
-        self.headers['Sec-Fetch-Dest'] = 'iframe'
-        self.headers['Sec-Fetch-Mode'] = 'navigate'
-        self.headers['Sec-Fetch-Site'] = 'same-origin'
-        self.headers['TE'] = 'trailers'
-        self.headers['Sec-Fetch-User'] = '?1'
 
         # 现在使用用户名，密码，验证码登录
         self.form_data['L_username'] = '500237201011301419'
@@ -155,11 +84,12 @@ class ModifypassworldSpider(scrapy.Spider):
         self.form_data['L_YZM'] = res
         self.form_data['Button1'] = '登录'
         bodystr = urlencode(self.form_data, encoding='utf-8')
-        self.headers['Content-Type'] = 'application/x-www-form-urlencoded; charset=utf-8'
-        self.headers['Content-Length'] = '{}'.format(len(bodystr))
+        headers = {}
+        headers['Content-Type'] = 'application/x-www-form-urlencoded; charset=utf-8'
+        headers['Content-Length'] = '{}'.format(len(bodystr))
         # 准备好了数据 按 登录 按钮
         yield scrapy.Request(url=self.user_dll_url, method='POST', body=bodystr,
-                             callback=self.login_parse, headers=self.headers, dont_filter=True)
+                             callback=self.login_parse, headers=headers, dont_filter=True)
 
     # 登录结果分析，登录结果中添加了 cookie XSQHUserName
     def login_parse(self, response):
@@ -170,46 +100,11 @@ class ModifypassworldSpider(scrapy.Spider):
             print('login fail')
             # 再次请求一个验证码
             print('try to get another yanzhengma')
-            self.headers['Referer'] = self.user_dll_url
-            self.headers['Accept'] = 'image/avif,image/webp,*/*'
-            self.headers['Sec-Fetch-Dest'] = 'image'
-            self.headers['Sec-Fetch-Mode'] = 'no-cors'
-            self.headers['Sec-Fetch-Site'] = 'same-origin'
-            self.headers['TE'] = 'trailers'
-            # 请求头中删除不需要的
-            if self.headers.get('Content-Type') is not None:
-                self.headers.pop('Content-Type')
-            if self.headers.get('Content-Length') is not None:
-                self.headers.pop('Content-Length')
-            if self.headers.get('Sec-Fetch-User') is not None:
-                self.headers.pop('Sec-Fetch-User')
-            if self.headers.get('Origin') is not None:
-                self.headers.pop('Origin')
-            if self.headers.get('Upgrade-Insecure-Requests') is not None:
-                self.headers.pop('Upgrade-Insecure-Requests')
-            if len(self.headers) != 12:
-                print('yzm rerequest possible headers is not correct, length of headers is:{}'.format(len(self.headers)))
-            yield scrapy.Request(url=self.yzm_url_full, callback=self.yzm_parse, headers=self.headers,
-                                     dont_filter=True)
+            yield scrapy.Request(url=self.yzm_url_full, callback=self.yzm_parse, dont_filter=True)
             return
         print('login success')
-        ckie = response.headers['Set-Cookie']
-        if str(ckie).find('XSQHUserName') < 0:
-            print('login fail too.')
-            return
-        fdf = str(ckie).split(';')
-        # 只需要有用的值
-        for i in fdf:
-            if i.find('XSQHUserName') >= 0:
-                self.headers['Cookie'] += ';' + i  # ASP.NET_SessionId=3rg5mw45ldudcbmsvfnyayj0
-                break
-        self.headers['Referer'] = response.url
         top_frame_url = urljoin(response.url, '../JW/JW_Top.aspx')
-        if self.headers.get('Content-Type') is not None:
-            self.headers.pop('Content-Type')
-        if self.headers.get('Content-Length') is not None:
-            self.headers.pop('Content-Length')
-        yield scrapy.Request(url=top_frame_url, callback=self.top_frame_parse, headers=self.headers, dont_filter=True)
+        yield scrapy.Request(url=top_frame_url, callback=self.top_frame_parse, dont_filter=True)
 
     def top_frame_parse(self, response):
         # 再响应中找到修改密码的链接
@@ -218,7 +113,7 @@ class ModifypassworldSpider(scrapy.Spider):
             return
         mpsd_url = mpsd.get()
         mpsd_url_full = urljoin(response.url, mpsd_url)
-        yield scrapy.Request(url=mpsd_url_full, callback=self.post_md_parse, headers=self.headers, dont_filter=True)
+        yield scrapy.Request(url=mpsd_url_full, callback=self.post_md_parse,  dont_filter=True)
 
     def post_md_parse(self, response):
         print('enter post_md_parse')
@@ -228,31 +123,26 @@ class ModifypassworldSpider(scrapy.Spider):
         self.form_data['__ASYNCPOST'] = 'true'
         self.form_data['Button1'] = '确认'
         self.form_data['Hselect'] = ''
-        event_argument = response.xpath('//div/input[@id="__EVENTARGUMENT"]/@value')
-        event_argument_str = event_argument.extract()[0] if len(event_argument) > 0 else ''
-        view_state = response.xpath('//div/input[@id="__VIEWSTATE"]/@value')
-        view_state_str = view_state.extract()[0] if len(view_state) > 0 else ''
-        event_validation = response.xpath('//div/input[@id="__EVENTVALIDATION"]/@value')
-        event_validation_str = event_validation.extract()[0] if len(event_validation) > 0 else ''
-        view_state_generator = response.xpath('//div/input[@id="__VIEWSTATEGENERATOR"]/@value')
-        view_state_generator_str = view_state_generator.extract()[0] if len(view_state_generator) > 0 else ''
+        event_argument = response.xpath('//input[@id="__EVENTARGUMENT"]/@value')
+        self.form_data['__EVENTARGUMENT'] = event_argument.extract()[0] if len(event_argument) > 0 else ''
+        view_state = response.xpath('//input[@id="__VIEWSTATE"]/@value')
+        self.form_data['__VIEWSTATE'] = view_state.extract()[0] if len(view_state) > 0 else ''
+        event_validation = response.xpath('//input[@id="__EVENTVALIDATION"]/@value')
+        self.form_data['__EVENTVALIDATION'] = event_validation.extract()[0] if len(event_validation) > 0 else ''
+        view_state_generator = response.xpath('//input[@id="__VIEWSTATEGENERATOR"]/@value')
+        self.form_data['__VIEWSTATEGENERATOR'] = view_state_generator.extract()[0] if len(view_state_generator) > 0 else ''
 
-        self.form_data['__EVENTARGUMENT'] = event_argument_str
-        self.form_data['__VIEWSTATE'] = view_state_str
-        self.form_data['__EVENTVALIDATION'] = event_validation_str
-        self.form_data['__VIEWSTATEGENERATOR'] = view_state_generator_str
         self.form_data['L_password1'] = 'a201025Q'
         self.form_data['L_password2'] = 'a201025Q'
         self.form_data['L_password3'] = 'a201025Q'
         self.form_data['TextBox_PWD'] = '17783631632'
         bodystr = urlencode(self.form_data, encoding='utf-8')
-        self.headers['Content-Type'] = 'application/x-www-form-urlencoded; charset=utf-8'
-        self.headers['Content-Length'] = '{}'.format(len(bodystr))
-        # yield scrapy.Request(url=action_url_full, method='POST', body=bodystr, callback=self.post_zsbm1_parse,
-        # headers=self.headers, dont_filter=True)
+        headers = {}
+        headers['Content-Type'] = 'application/x-www-form-urlencoded; charset=utf-8'
+        headers['Content-Length'] = '{}'.format(len(bodystr))
         print('exit post_md_parse')
         yield scrapy.Request(url=response.url, method='POST', body=bodystr, callback=self.que_ren_parse,
-                             headers=self.headers, dont_filter=True)
+                             headers=headers, dont_filter=True)
     def que_ren_parse(self, response):
         with open('que_ren.txt', 'w') as f:
             f.write(response.text)
