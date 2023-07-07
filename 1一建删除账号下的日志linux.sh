f13e4ad2769f483a72ca946f817fe197ea2bb0b7
jingshaoqi <jingshaoqi@163.com>
2023-07-07 15:11:37 +0800

    添加windows下的脚本

---
+++ 1一建删除账号下的日志linux.sh
@@ -0,0 +1,31 @@
+cd junior_qianghao-hemeng
+del /F /Q logging.log
+cd ..
+
+cd junior_qianghao-hujingbo
+del /F /Q logging.log
+cd ..
+
+cd junior_qianghao-malinlin
+del /F /Q logging.log
+cd ..
+
+cd junior_qianghao-zhengmenghan
+del /F /Q logging.log
+cd ..
+
+cd junior_qianghao-zhangyangcheng
+del /F /Q logging.log
+cd ..
+
+cd junior_qianghao-fengqing
+del /F /Q logging.log
+cd ..
+
+cd junior_qianghao-pengguangxiong
+del /F /Q logging.log
+cd ..
+
+cd junior_qianghao-tanyinhao
+del /F /Q logging.log
+cd ..
